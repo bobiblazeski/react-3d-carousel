edd63c47a6e34ba9d2ddade0e526e8530c50c444
Slobodan Blazeski <slobodan.blazeski@gmail.com>
2015-05-13 21:05:05 +0200

    Updated package json versions

--- example/bundle.js
+++ example/bundle.js
@@ -61,8 +61,8 @@
 	'use strict';
 
 	var Carousel = __webpack_require__(2);
-	var Ease = __webpack_require__(8);
-	var images = __webpack_require__(9);
+	var Ease = __webpack_require__(7);
+	var images = __webpack_require__(8);
 
 	var MainView = React.createClass({
 	    displayName: 'MainView',
@@ -558,162 +558,6 @@
 /* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
-
-	var _exports = module.exports = {};
-
-	function bounceOut(time, begin, change, duration) {
-	    if ((time /= duration) < 1 / 2.75) {
-	        return change * (7.5625 * time * time) + begin;
-	    } else if (time < 2 / 2.75) {
-	        return change * (7.5625 * (time -= 1.5 / 2.75) * time + 0.75) + begin;
-	    } else if (time < 2.5 / 2.75) {
-	        return change * (7.5625 * (time -= 2.25 / 2.75) * time + 0.9375) + begin;
-	    } else {
-	        return change * (7.5625 * (time -= 2.625 / 2.75) * time + 0.984375) + begin;
-	    }
-	}
-	_exports.bounceOut = bounceOut;
-
-	function bounceIn(time, begin, change, duration) {
-	    return change - bounceOut(duration - time, 0, change, duration) + begin;
-	}
-	_exports.bounceIn = bounceIn;
-
-	_exports.bounceInOut = function bounceInOut(time, begin, change, duration) {
-	    if (time < duration / 2) {
-	        return bounceIn(time * 2, 0, change, duration) * 0.5 + begin;
-	    } else {
-	        return bounceOut(time * 2 - duration, 0, change, duration) * 0.5 + change * 0.5 + begin;
-	    }
-	};
-
-	_exports.circIn = function circIn(time, begin, change, duration) {
-	    return -change * (Math.sqrt(1 - (time = time / duration) * time) - 1) + begin;
-	};
-
-	_exports.circOut = function circOut(time, begin, change, duration) {
-	    return change * Math.sqrt(1 - (time = time / duration - 1) * time) + begin;
-	};
-
-	_exports.circInOut = function circInOut(time, begin, change, duration) {
-	    if ((time = time / (duration / 2)) < 1) {
-	        return -change / 2 * (Math.sqrt(1 - time * time) - 1) + begin;
-	    } else {
-	        return change / 2 * (Math.sqrt(1 - (time -= 2) * time) + 1) + begin;
-	    }
-	};
-
-	_exports.cubicIn = function cubicIn(time, begin, change, duration) {
-	    return change * (time /= duration) * time * time + begin;
-	};
-
-	_exports.cubicOut = function cubicOut(time, begin, change, duration) {
-	    return change * ((time = time / duration - 1) * time * time + 1) + begin;
-	};
-
-	_exports.cubicInOut = function cubicInOut(time, begin, change, duration) {
-	    if ((time = time / (duration / 2)) < 1) {
-	        return change / 2 * time * time * time + begin;
-	    } else {
-	        return change / 2 * ((time -= 2) * time * time + 2) + begin;
-	    }
-	};
-
-	_exports.expoIn = function expoIn(time, begin, change, duration) {
-	    if (time === 0) {
-	        return begin;
-	    }
-	    return change * Math.pow(2, 10 * (time / duration - 1)) + begin;
-	};
-
-	_exports.expoOut = function expoOut(time, begin, change, duration) {
-	    if (time === duration) {
-	        return begin + change;
-	    }
-	    return change * (-Math.pow(2, -10 * time / duration) + 1) + begin;
-	};
-
-	_exports.expoInOut = function expoInOut(time, begin, change, duration) {
-	    if (time === 0) {
-	        return begin;
-	    } else if (time === duration) {
-	        return begin + change;
-	    } else if ((time = time / (duration / 2)) < 1) {
-	        return change / 2 * Math.pow(2, 10 * (time - 1)) + begin;
-	    } else {
-	        return change / 2 * (-Math.pow(2, -10 * (time - 1)) + 2) + begin;
-	    }
-	};
-
-	_exports.linear = function linear(time, begin, change, duration) {
-	    return change * time / duration + begin;
-	};
-
-	_exports.quadIn = function quadIn(time, begin, change, duration) {
-	    return change * (time = time / duration) * time + begin;
-	};
-
-	_exports.quadOut = function quadOut(time, begin, change, duration) {
-	    return -change * (time = time / duration) * (time - 2) + begin;
-	};
-
-	_exports.quadInOut = function quadInOut(time, begin, change, duration) {
-	    if ((time = time / (duration / 2)) < 1) {
-	        return change / 2 * time * time + begin;
-	    } else {
-	        return -change / 2 * ((time -= 1) * (time - 2) - 1) + begin;
-	    }
-	};
-
-	_exports.quartIn = function quartIn(time, begin, change, duration) {
-	    return change * (time = time / duration) * time * time * time + begin;
-	};
-
-	_exports.quartOut = function quartOut(time, begin, change, duration) {
-	    return -change * ((time = time / duration - 1) * time * time * time - 1) + begin;
-	};
-
-	_exports.quartInOut = function quartInOut(time, begin, change, duration) {
-	    if ((time = time / (duration / 2)) < 1) {
-	        return change / 2 * time * time * time * time + begin;
-	    } else {
-	        return -change / 2 * ((time -= 2) * time * time * time - 2) + begin;
-	    }
-	};
-
-	_exports.quintIn = function quintIn(time, begin, change, duration) {
-	    return change * (time = time / duration) * time * time * time * time + begin;
-	};
-
-	_exports.quintOut = function quintOut(time, begin, change, duration) {
-	    return change * ((time = time / duration - 1) * time * time * time * time + 1) + begin;
-	};
-
-	_exports.quintInOut = function quintInOut(time, begin, change, duration) {
-	    if ((time = time / (duration / 2)) < 1) {
-	        return change / 2 * time * time * time * time * time + begin;
-	    } else {
-	        return change / 2 * ((time -= 2) * time * time * time * time + 2) + begin;
-	    }
-	};
-
-	_exports.sineIn = function sineIn(time, begin, change, duration) {
-	    return -change * Math.cos(time / duration * (Math.PI / 2)) + change + begin;
-	};
-
-	_exports.sineOut = function sineOut(time, begin, change, duration) {
-	    return change * Math.sin(time / duration * (Math.PI / 2)) + begin;
-	};
-
-	_exports.sineInOut = function sineInOut(time, begin, change, duration) {
-	    return -change / 2 * (Math.cos(Math.PI * time / duration) - 1) + begin;
-	};
-
-/***/ },
-/* 8 */
-/***/ function(module, exports, __webpack_require__) {
-
 	(function () {
 
 	    'use strict';
@@ -907,7 +751,7 @@
 	}.call(this));
 
 /***/ },
-/* 9 */
+/* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// Sample images used in the demo
